0f2c58e28109a36c31526ddf3b149e03d7901a55
Daniel <dgtutv@gmail.com>
2023-08-15 22:05:29 -0700

    Completed playRound function

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -11,3 +11,25 @@ let getComputerChoice = function(){
         return "Scissors"
     }
 }
+
+let playRound = function(playerSelection, computerSelection){
+    console.log("Player selection: "+playerSelection);
+    console.log("Computer selection: "+computerSelection);
+    if(playerSelection.toLowerCase() == computerSelection.toLowerCase()){
+        return "Draw";
+    }
+    else{
+        if(playerSelection.toLowerCase() == "rock"){
+            if(computerSelection == "Paper"){return "Computer Wins"};
+            return "Player Wins";
+        }
+        else if(playerSelection.toLowerCase() == "paper"){
+            if(computerSelection == "Scissors"){return "Computer Wins"};
+            return "Player Wins";
+        }
+        else{
+            if(computerSelection == "Rock"){return "Computer Wins"};
+            return "Player Wins";
+        }
+    }
+}
