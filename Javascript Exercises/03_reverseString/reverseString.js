f68effaa8e908564dc727542a932f68ddecba674
Daniel <dgtutv@gmail.com>
2023-08-21 18:26:11 -0700

    Completed the reverseString exercise

--- Javascript Exercises/03_reverseString/reverseString.js
+++ Javascript Exercises/03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(originalString) {
+    splitString = originalString.split("");
+    let reverseString="";
+    for(let i=splitString.length-1; i>-1; i--){
+        reverseString = reverseString+splitString[i];
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
