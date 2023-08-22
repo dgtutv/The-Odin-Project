7a078bf17d3b5c32955fddd62800805f70831774
Daniel <dgtutv@gmail.com>
2023-08-21 18:41:06 -0700

    Issue with item iteration in array in removeFromArray fuction

--- Javascript Exercises/04_removeFromArray/removeFromArray.js
+++ Javascript Exercises/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeItemFromArray = function(array, item){
+    let index = array.indexOf(item);
+    if(index > -1){
+        array.splice(index, 1);
+    }
+    return array;
+}
 
+const removeFromArray = function(array, ...args) {
+    for(element in args){
+        array = removeItemFromArray(array, element);
+    }
+    return array;
 };
 
 // Do not edit below this line
