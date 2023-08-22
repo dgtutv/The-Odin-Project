06eb8c3dee5f79f86efd426987ca1f6e6668ad63
Daniel <dgtutv@gmail.com>
2023-08-21 18:54:39 -0700

    Finished Exercise

--- Javascript Exercises/05_sumAll/sumAll.js
+++ Javascript Exercises/05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if(end<start){
+        let temp=end;
+        end=start;
+        start=temp;
+    }
+    if(start<0 || end<0){
+        return "ERROR";
+    }
+    if(typeof(start) != "number" || typeof(end) != "number"){
+        return "ERROR";
+    }
+    let sum=start;
+    for(let i=start+1; i<end+1; i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
