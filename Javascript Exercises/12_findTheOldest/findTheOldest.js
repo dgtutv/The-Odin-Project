a908beac5f19801bc880c1acbfeecc30dad94198
Daniel <dgtutv@gmail.com>
2023-09-29 22:06:29 -0700

    Completed Exercise

--- Javascript Exercises/12_findTheOldest/findTheOldest.js
+++ Javascript Exercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldestPerson = people[0];
+    let currentPerson;
+    for(let i=0; i<people.length; i++){
+        currentPerson=people[i];
+        if(!("yearOfDeath" in currentPerson)){
+            let currentDate = new Date();
+            currentPerson.age =  currentDate.getFullYear() - currentPerson.yearOfBirth;
+        }
+        else{
+            currentPerson.age = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+        }
+        if(currentPerson.age > oldestPerson.age){
+            oldestPerson = currentPerson;
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
