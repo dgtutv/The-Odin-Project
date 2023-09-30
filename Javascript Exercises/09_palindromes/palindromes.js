a8ec8ac39ae17cad74025eaf595086076ca38d57
Daniel <dgtutv@gmail.com>
2023-09-29 21:18:05 -0700

    Completed Exercise

--- Javascript Exercises/09_palindromes/palindromes.js
+++ Javascript Exercises/09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.replace(/[^\w\s]/g, '');    //Remove punctuation
+    string = string.replace(/\s+/g, '');    //Remove white space
+    string = string.toLowerCase();
+    let forwardCounter = 0;
+    for(let i=string.length-1; i>-1; i--){
+        if(string[forwardCounter] != string[i]){
+            return false;
+        }
+        forwardCounter++;
+    }
+    return true;
 };
 
 // Do not edit below this line
