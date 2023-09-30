681a5c35092717606407d70ecf48d1384f6cc7d1
Daniel <dgtutv@gmail.com>
2023-09-29 21:45:26 -0700

    Working on why function does not work for 8 as a string input

--- Javascript Exercises/10_fibonacci/fibonacci.js
+++ Javascript Exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if(index < 0){
+        return("OOPS");
+    }
+    else if(index == 0){
+        return 0;
+    }
+    else if(index == 1){
+        return 1;
+    }
+    else if(index  == 2){
+        return 1;
+    }
+    else{
+        let prevNum=0;
+        let currNum=1;
+        for(let i=2; i<index+1; i++){
+            nextNum=currNum+prevNum;
+            console.log(`prevNum: ${prevNum}\ncurrNum: ${currNum}\n nextNum: ${nextNum}`);
+            prevNum=currNum;
+            currNum=nextNum;
+        }
+        return nextNum;
+    }
 };
 
 // Do not edit below this line
