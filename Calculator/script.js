d1f7aa305bb256fc1f49e2e3cc6f321d7f5b7172
Daniel Todd <dgtutv@gmail.com>
2023-12-06 13:49:15 -0800

    Started work on site logic

---
+++ Calculator/script.js
@@ -0,0 +1,9 @@
+let display = document.querySelector("#display");
+
+
+document.querySelectorAll('.number').forEach((button) => {
+    button.addEventListener('click', () => {
+        display.textContent += button.id;
+        
+    });
+});
