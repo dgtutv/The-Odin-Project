0034c9619316008f6be1c3a19e82d51ad3360584
Daniel Todd <dgtutv@gmail.com>
2023-12-06 14:04:56 -0800

    Continuing work on javascript

--- Calculator/script.js
+++ Calculator/script.js
@@ -4,6 +4,6 @@ let display = document.querySelector("#display");
 document.querySelectorAll('.number').forEach((button) => {
     button.addEventListener('click', () => {
         display.textContent += button.id;
-        
+        console.log(button.id);
     });
 });
