de5d39f6c209bdde4f19a4beef7800b0792fa7de
Daniel Todd <dgtutv@gmail.com>
2024-05-02 17:09:31 -0700

    Passwords do not match not dynamically appears and re-appears when needed

--- Sign Up Form/script.js
+++ Sign Up Form/script.js
@@ -2,6 +2,27 @@ let button = document.querySelector("button");
 let password = document.querySelector("#pwd");
 let confirmPassword = document.querySelector("#confirmPwd");
 let form = document.querySelector("form");
+let passwordsNotMatch = document.querySelector("#pwdNotMatch");
+password.addEventListener('change', function(event){
+    if(password.value === confirmPassword.value){
+        passwordsNotMatch.classList.add("hidden");
+    }
+    else{
+        if(passwordsNotMatch.classList.contains("hidden")){
+            passwordsNotMatch.classList.remove("hidden");
+        }    
+    }
+});
+confirmPassword.addEventListener('change', function(event){
+    if(password.value === confirmPassword.value){
+        passwordsNotMatch.classList.add("hidden");
+    }
+    else{
+        if(passwordsNotMatch.classList.contains("hidden")){
+            passwordsNotMatch.classList.remove("hidden");
+        }    
+    }
+});
 button.addEventListener('click', function(event){
     if(password.value === confirmPassword.value){
         form.submit();
