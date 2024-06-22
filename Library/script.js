0a96126356187035c5c4d41bc6005c79933c62b3
Daniel Todd <dgtutv@gmail.com>
2024-06-22 15:47:01 -0700

    Added delete functionality

--- Library/script.js
+++ Library/script.js
@@ -42,8 +42,8 @@ function addBook(newBook){
     icons.classList.add("icons");
     newCard.appendChild(icons);
 
-    const pencilCode = '<svg id="pencil" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>pencil</title><path d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" /></svg>';
-    const trashCode = '<svg id="trash" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>trash-can</title><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,8H11V17H9V8M13,8H15V17H13V8Z" /></svg>';
+    const pencilCode = '<svg id="pencil" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Change Status</title><path d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" /></svg>';
+    const trashCode = '<svg id="trash" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Delete Card</title><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,8H11V17H9V8M13,8H15V17H13V8Z" /></svg>';
     
     let pencil = document.createElement('div');
     pencil.innerHTML = pencilCode;
@@ -61,7 +61,7 @@ function addBook(newBook){
         console.log(`Card#${newBook.id} edit`);
     });
     trashSvg.addEventListener("click", function(e){
-        console.log(`Card#${newBook.id} delete`);
+        newCard.remove();
     });
 }
 
