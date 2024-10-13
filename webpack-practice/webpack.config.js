4a5067913a2dd0329233eb8ab7e6dafc4e3a4466
Daniel Todd <dgtutv@gmail.com>
2024-10-13 15:20:53 -0700

    Added html loader for images

--- webpack-practice/webpack.config.js
+++ webpack-practice/webpack.config.js
@@ -16,11 +16,19 @@ module.exports = {
         }),
     ],
     module: {
-    rules: [
-        {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-        },
-    ],
+        rules: [
+            {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
     },
 };
