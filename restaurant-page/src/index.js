85facc932cdc13ee015383c686e6525d7e8dde53
Daniel Todd <dgtutv@gmail.com>
2024-10-18 15:34:14 -0700

    Moved home page content to loadPage function

--- restaurant-page/src/index.js
+++ restaurant-page/src/index.js
@@ -1,1 +1,25 @@
 import "./styles.css";
+
+function loadPage(){
+    const content = document.querySelector("#content");
+    content.innerHTML =  `<div class="imageBackground" id="homeImage">
+            <div class="imageText">
+                <h1>Where food meets art.</h1>
+                <h2>Endulge in meals that enforce healthy highs of mystical flavour.</h2>
+            </div>
+        </div>
+        <div class="imageBackground" id="cheeseBurger">
+            <div class="imageText">
+                <h1>American Classic Cheeseburger.</h1>
+                <h2>Built with bold cheddar and a juicy patty.</h2>
+            </div>
+        </div>
+        <div class="imageBackground" id="caesarSalad">
+            <div class="imageText">
+                <h1>Roman Caesar Salad.</h1>
+            <h2>The finest dressing in town meets crisp romaine from our garden.</h2>
+            </div>
+        </div>`;
+}
+
+loadPage();
