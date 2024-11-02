ef88201e2fcc0d43f678c34e2353081620df39c9
Daniel <dgtutv@gmail.com>
2024-11-02 15:45:23 -0700

    Working on basic structure of list objects

--- todo-list/src/list.js
+++ todo-list/src/list.js
@@ -1,7 +1,17 @@
 export default List;
+import Todo from "./todo.js"
 
 class List{
     constructor(id){
-        
+        this.id = id;
+        this.todos = []     //Our array of todo items
+    }
+
+    add(){
+
+    }
+
+    remove(id){     //ID of the todo to remove
+
     }
 }
