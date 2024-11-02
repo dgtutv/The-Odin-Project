d57d4de4ddfdbe0c6cf7b3df7f93e98de76d5751
Daniel <dgtutv@gmail.com>
2024-11-02 15:41:49 -0700

    Added isPresent method to data for checking the existence of lists

--- todo-list/src/data.js
+++ todo-list/src/data.js
@@ -1,17 +1,27 @@
 import List from "./list.js";
+export default Data;
 
-
-//TODO: prevent user from creating two lists with the same name
+//TODO: prevent user from creating two lists with the same name, cant do in this class (use isPresent)
 class Data{
     pull(id){
-        if(localStorage.getItem(id) !== null){
+        if(this.isPresent(id)){
             return JSON.parse(localStorage.getItem(id));
         }
         else{
             return new List(id);
         }
     }
+
     push(list){
         localStorage.setItem(`${list.id}`, JSON.stringify(list));
+    }
+
+    isPresent(id){
+        if(localStorage.getItem(id) !== null){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
