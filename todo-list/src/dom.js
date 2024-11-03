451e0772393f2072d5e20aa101fbd0b66e9a995e
Daniel <dgtutv@gmail.com>
2024-11-03 15:55:56 -0800

    Working on dom manipulation class

---
+++ todo-list/src/dom.js
@@ -0,0 +1,14 @@
+export default class DOM{
+    contentArea = document.querySelector("#content");
+    addTaskBtn = document.querySelector("#addTask");
+    addProjectBtn = document.querySelector("#addProject");
+    renderContents(list){
+
+    }
+    destructContents(){
+
+    }
+    renderLists(){  //Render the custom lists section
+
+    }
+}
--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 import List from "./list.js";
 import Data from "./data.js";
+import DOM from "./dom.js";
 
 //If first time starting application, create the legacy list sections
 let dataHandler = new Data(List);
@@ -8,11 +9,11 @@ let myDay = dataHandler.pull("myDay");
 let important = dataHandler.pull("important");
 let allTodos = dataHandler.pull("allTodos");
 
-//Render all custom lists
 
-//Start off in My Day section
+//Start off in My Day section, render its contents
+let domHandler = new DOM;
 let currentList = myDay;
-
+domHandler.render(currentList);
 
 //TODO: prevent user from creating two lists with the same name, cant do in this class (use isPresent)
 
