9b5b7cc69253e229fd41695ac1f94d9c11ef0e0f
Daniel <dgtutv@gmail.com>
2024-11-16 13:11:46 -0800

    Added priority list dropdown to new task menu

--- todo-list/src/dom.js
+++ todo-list/src/dom.js
@@ -12,13 +12,23 @@ export default class DOM{
                 Priority dropdown; low, medium, high
                 Checklist, user enters in checkbox, each time they press enter it gets added to the list
             */
-            const checkList = document.querySelector("#projectList");
-            checkList.getElementsByClassName('projectAnchor')[0].onclick = function(e){
-                if (checkList.classList.contains('visible')){
-                    checkList.classList.remove('visible');
+            const projectList = document.querySelector("#projectList");
+            projectList.getElementsByClassName('projectAnchor')[0].onclick = function(e){
+                if (projectList.classList.contains('visible')){
+                    projectList.classList.remove('visible');
                 }
                 else{
-                    checkList.classList.add('visible');
+                    projectList.classList.add('visible');
+                }              
+            }
+
+            const priorityList = document.querySelector("#priorityList");
+            priorityList.getElementsByClassName('projectAnchor')[0].onclick = function(e){
+                if (priorityList.classList.contains('visible')){
+                    priorityList.classList.remove('visible');
+                }
+                else{
+                    priorityList.classList.add('visible');
                 }              
             }
         });
