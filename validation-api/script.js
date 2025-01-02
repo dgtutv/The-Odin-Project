a85f4da36a156bf03034d791a5b0f1a4985a8f3e
Daniel Todd <dgtutv@gmail.com>
2025-01-02 14:10:49 -0800

    Added email validity

---
+++ validation-api/script.js
@@ -0,0 +1,10 @@
+const email = document.querySelector("#mail");
+email.addEventListener('input', (event) =>{
+    if(email.validity.typeMismatch){
+        email.setCustomValidity("Enter a valid email address!");
+    }
+    else{
+        email.setCustomValidity("");
+    }
+    email.reportValidity();
+});
