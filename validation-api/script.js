11d266a6ca99f3e7d40a4eb5797e9a7bb4edb8a3
Daniel Todd <dgtutv@gmail.com>
2025-01-04 00:21:21 -0800

    Finished assignment

--- validation-api/script.js
+++ validation-api/script.js
@@ -1,75 +1,96 @@
 const email = document.querySelector("#mail");
-email.addEventListener('input', (event) =>{
-    if(email.validity.typeMismatch){
+email.addEventListener('input', emailValidity);
+
+function emailValidity() {
+    let isValid = true;
+    if (email.validity.typeMismatch) {
         email.setCustomValidity("Enter a valid email address.");
-    }
-    else{
+        isValid = false;
+    } else {
         email.setCustomValidity("");
     }
     email.reportValidity();
-});
+    return isValid;
+}
 
 const country = document.querySelector("#country");
-country.addEventListener('input', (event) =>{
-    if(country.validity.valueMissing){
-        country.setCustomValidity("Country is required.")
-    }
-    else if(country.validity.tooShort){
+country.addEventListener('input', countryValidity);
+
+function countryValidity() {
+    let isValid = true;
+    if (country.validity.valueMissing) {
+        country.setCustomValidity("Country is required.");
+        isValid = false;
+    } else if (country.validity.tooShort) {
         country.setCustomValidity("Country length must be longer than 1.");
-    }
-    else if(country.validity.tooLong){
+        isValid = false;
+    } else if (country.validity.tooLong) {
         country.setCustomValidity("Country length must be shorter than 30.");
-    }
-    else{
+        isValid = false;
+    } else {
         country.setCustomValidity("");
     }
     country.reportValidity();
-});
+    return isValid;
+}
 
 const postalCode = document.querySelector("#postalCode");
-postalCode.addEventListener('input', (event)=>{
-    if(postalCode.validity.valueMissing){
+postalCode.addEventListener('input', postalCodeValidity);
+
+function postalCodeValidity() {
+    let isValid = true;
+    if (postalCode.validity.valueMissing) {
         postalCode.setCustomValidity("Postal code is required.");
-    }
-    else if(postalCode.validity.patternMismatch){
+        isValid = false;
+    } else if (postalCode.validity.patternMismatch) {
         postalCode.setCustomValidity("Enter a valid postal code.");
-    }
-    else{
+        isValid = false;
+    } else {
         postalCode.setCustomValidity("");
     }
     postalCode.reportValidity();
-});
+    return isValid;
+}
 
 const password = document.querySelector("#password");
-password.addEventListener('input', (event) =>{
-    if(password.validity.valueMissing){
-        password.setCustomValidity("Password is required.")
-    }
-    else if(password.validity.tooShort){
+password.addEventListener('input', passwordValidity);
+
+function passwordValidity() {
+    let isValid = true;
+    if (password.validity.valueMissing) {
+        password.setCustomValidity("Password is required.");
+        isValid = false;
+    } else if (password.validity.tooShort) {
         password.setCustomValidity("Password length must be longer than 1.");
-    }
-    else if(password.validity.tooLong){
+        isValid = false;
+    } else if (password.validity.tooLong) {
         password.setCustomValidity("Password length must be shorter than 30.");
-    }
-    else{
+        isValid = false;
+    } else {
         password.setCustomValidity("");
     }
     password.reportValidity();
-});
+    return isValid;
+}
 
 const confirmPassword = document.querySelector("#confirmPassword");
-confirmPassword.addEventListener('input', (event) =>{
-    if(password.value != confirmPassword.value){
+confirmPassword.addEventListener('input', confirmPasswordValidity);
+
+function confirmPasswordValidity() {
+    let isValid = true;
+    if (password.value != confirmPassword.value) {
         confirmPassword.setCustomValidity("Passwords must be the same.");
-    }
-    else{
+        isValid = false;
+    } else {
         confirmPassword.setCustomValidity("");
     }
     confirmPassword.reportValidity();
-});
+    return isValid;
+}
 
-const submitBtn = document.querySelector("button");
-submitBtn.addEventListener('click', (event) =>{
-    let isValid = true;
-    //Check all validations, if failed show message. Otherwise submit. If not work, change to form submit rather than button
-})
+const form = document.querySelector("form");
+form.addEventListener('submit', (event) => {
+    if (!emailValidity() || !countryValidity() || !postalCodeValidity() || !passwordValidity() || !confirmPasswordValidity()) {
+        event.preventDefault();
+    }
+});
