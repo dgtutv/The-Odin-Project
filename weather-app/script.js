dfc04f7fc3f0f3b0f281978e6969b1cca8bb2d79
Daniel Todd <dgtutv@gmail.com>
2025-01-05 16:30:46 -0800

    Added custom validity errors to search bar

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,5 +1,9 @@
+//Global Variables
 const apiKey = 'WDVN9BJ4YDFAK7H8BNLJX64N4';
+const searchBar = document.querySelector("#searchBar");
+const searchButton = document.querySelector("#searchButton");
 
+//Functions
 async function getLocationData(city, country) {
     const URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city},${country}?key=${apiKey}`;
     try{
@@ -15,14 +19,28 @@ async function getLocationData(city, country) {
     }
 }
 
-document.querySelector("#searchButton").addEventListener('click', (event) => {
-    const location = document.querySelector("#searchBar").value.split(",");
+function searchBarValidity(){
+    let isValid = true;
+    const location = searchBar.value.split(",");
     if(location.length != 2){
         console.log("ERROR");
+        searchBar.setCustomValidity("Location must be in format \"City, Country\"");
     }
+    else{
+        searchBar.setCustomValidity("");
+    }
+    searchBar.reportValidity();
+}
+
+//Event Listeners
+searchButton.addEventListener('click', (event) => {
+    const location = searchBar.value.split(",");
+    searchBarValidity();
     console.log(location);
 });
 
+
+//Main
 getLocationData("Abbotsford", "Canada").then((result) => {
     console.log(result);
 });
