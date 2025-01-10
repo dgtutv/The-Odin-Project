62d27d6617f333ecbc47c210b2a1ff2fd97003fa
Daniel Todd <dgtutv@gmail.com>
2025-01-09 23:44:09 -0800

    Added prepend, size, head, tail, at(index) methods

--- linked-list/script.js
+++ linked-list/script.js
@@ -31,12 +31,71 @@ class LinkedList{
         }
     }
 
-    
+    prepend(value){
+        let newNode = new Node;
+        newNode.value = value;
+        newNode.nextNode = this.root;
+        this.root = newNode;
+    }
+
+    size(){
+        let currentNode = this.root;
+        let counter = 0;
+        while(currentNode != null){
+            currentNode = currentNode.nextNode;
+            counter++;
+        }
+        console.log(counter);
+        return counter;
+    }
+
+    head(){
+        return this.root;
+    }
+
+    tail(){
+        let currentNode = this.root;
+        while(currentNode.nextNode != null){
+            currentNode = currentNode.nextNode;
+        }
+        return currentNode;
+    }
+
+    at(index){
+        if(index <= 0){
+            console.log(this.root);
+            return this.root;
+        }
+        let currentNode = this.root;
+        for(let i=0; i<index; i++){
+            if(currentNode == null){
+                return null;
+            }
+            currentNode = currentNode.nextNode;
+        }
+        console.log(currentNode);
+        return currentNode;
+    }
 }
 
 let list = new LinkedList;
 list.toString();
+list.size();
 list.append(4);
 list.toString();
+list.size();
 list.append(7);
 list.toString();
+list.size();
+console.log(list.head());
+list.prepend(2);
+console.log(list.head());
+list.toString();
+list.size();
+console.log(list.tail());
+list.toString();
+console.log("here");
+list.at(0);
+list.at(1);
+list.at(2);
+list.at(3);
