a7d01b44b02834dcda0f1e76accf42af4aac5eca
Daniel Todd <dgtutv@gmail.com>
2025-03-28 16:57:46 -0700

    Finally got height of App to correct 100%

--- cv-builder/src/App.jsx
+++ cv-builder/src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
-import './App.css'
 import Section from './Section'
 import { Box } from '@mui/material'
 
 function App() {
   const [activeSection, setActiveSection] = useState(0)
 
-  const style = {
-    backgroundColor: "#ecedef",
+  const inputStyle = {
     padding: "24px",
     height: "100%",
     display: "flex",
@@ -17,11 +15,24 @@ function App() {
     gap: '12px',
   }
 
+  const globalStyle = {
+    backgroundColor: "#ecedef",
+    height: "100%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    gap: '12px',
+    minHeight: '100vh',
+  }
+
   return (
-    <Box sx={style}>
-      <Section type="details" isActive />
-      <Section type="education" />
-      <Section type="experience" />
+    <Box sx={globalStyle}>
+      <Box sx={inputStyle}>
+        <Section type="details" isActive />
+        <Section type="education" />
+        <Section type="experience" />
+      </Box>
     </Box>
   )
 }
--- cv-builder/src/main.jsx
+++ cv-builder/src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import "./index.css"
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
+  <StrictMode sx={{margin: "0", padding: "0", height: "100%"}}>
+    <App sx={{height: "100%"}} />
   </StrictMode>,
 )
